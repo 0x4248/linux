84e80936bc35f06d8e46b5af364dcddf33da98fb
0x4248 <60709927+0x4248@users.noreply.github.com>
2026-03-26 23:16:42 +0000

    initramfs: update init

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 qemu-system-x86_64 \
   -kernel arch/x86_64/boot/bzImage \
-  -initrd initramfs/initramfs.cpio.gz \
-  -m 512M \
-  -vga cirrus \
-  -append "console=ttyS0,115200n8 console=tty0 fbcon=map:0 video=vesafb:mtrr:3,ywrap,1024x768-16 root=/dev/ram rdinit=/init loglevel=7"
+  -initrd initramfs/initramfs.cpio \
+  -append "console=ttyS0 console=tty0 init=/bin/sh video=640x480" \
+  -serial mon:stdio \
+  -device virtio-gpu-pci \
+  -display sdl 
